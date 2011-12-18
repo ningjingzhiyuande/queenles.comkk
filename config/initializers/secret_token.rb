fa55fcac3b82016766529684fccb0334fab53b25
Wade Xing <iamxingxing@gmail.com>
2011-12-18 22:15:50 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Qles::Application.config.secret_token = 'bc0e1d93937f56816de846f5d5cef5d67a71b5f8f74a00c0123c73ea17ae72ef73f6fc4d5e565bfda9acc3c67bae81f02851cc9bd375ad535360fe57c8c8c3e3'
